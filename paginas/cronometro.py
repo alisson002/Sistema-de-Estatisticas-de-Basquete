effb9aa101986bd6cc040c8df34efe647506b784
Alisson Moreira <alisson-mu@hotmail.com>
2025-09-23 15:57:36 -0300

    ajustando tamanhos de fonte

--- paginas/cronometro.py
+++ paginas/cronometro.py
@@ -289,7 +289,7 @@ def main():
             /* Responsividade para telas menores - 5% menor que as proporções originais */
             @media (max-width: 768px) {
                 html, body {
-                    margin-top: -70px; /* 5% menor que -100px original */
+                    margin-top: -130px; /* 5% menor que -100px original */
                     height: 100vh; /* Volta para 100vh no mobile */
                 }
 
