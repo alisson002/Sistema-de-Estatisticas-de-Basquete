f27115cbc9324f41acbf5f02988d79a692537c86
Alisson Moreira <alisson-mu@hotmail.com>
2025-09-23 16:01:40 -0300

    ajustando tamanhos de fonte

--- paginas/cronometro.py
+++ paginas/cronometro.py
@@ -387,7 +387,7 @@ def main():
                 }
 
                 .timer-text {
-                    font-size: 75px; /* 5% menor que 105px original */
+                    font-size: 85px; /* 5% menor que 105px original */
                     letter-spacing: 2.85px; /* 5% menor que 3px original */
                 }
                 
